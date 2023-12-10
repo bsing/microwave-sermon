da0efccdaed6b0d59f5092878a90df0b41e59046
bsing <bsing@users.noreply.github.com>
2023-12-10 11:10:38 +1000

    Update createHtml.js

--- scripts/createHtml.js
+++ scripts/createHtml.js
@@ -1,1 +1,22 @@
+function generateHtml() {
+  let htmlBoxes = "";
+
+  numbers.forEach((number) => {
+    htmlBoxes += `<div class="number-box">
+    <div class="box-header">
+      <div class="box-image">
+        <img src="${number.image}" />
+      </div>
+      <div class="box-number">${number.number}</div>
+    </div>
+    <div class="box-text">
+      ${number.htmlText}
+    </div>
+    </div>`;
+  });
+
+  document.querySelector(".main").innerHTML = htmlBoxes;
+}
+
+generateHtml();
 
